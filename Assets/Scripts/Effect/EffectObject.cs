6c40487988ce36e9b3925d14bf914863fb3cb9f7
yougong0215 <yougong0215@gmail.com>
2024-01-03 12:57:52 +0900

    이팩트 관련 푸쉬

---
+++ Assets/Scripts/Effect/EffectObject.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectObject : PoolAble
+{
+    private ParticleSystem _particle;
+    private bool _init = false;
+
+    public ParticleSystem Particle
+    {
+        get
+        {
+            if (_particle)
+                _particle = GetComponent<ParticleSystem>();
+            
+            return _particle;
+        }
+    }
+
+    public override void Reset()
+    {
+        Particle.Stop();
+        _init = false;
+    }
+
+    public void Init(Vector3 pos, Quaternion qut)
+    {
+        transform.position = pos;
+        transform.rotation = qut;
+        
+        Particle.Play();
+        _init = true;
+    }
+    
+    public void Init(Transform parent)
+    {
+        transform.position = parent.position;
+        transform.rotation = parent.rotation;
+        
+        Particle.Play();
+        _init = true;
+    }
+    private void Update()
+    {
+        if (_init && Particle.isPlaying == false)
+        {
+            Particle.Stop();
+            PoolManager.Instance.Push(this);
+        }
+    }
+}
